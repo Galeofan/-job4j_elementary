78f8bb6d8f627b692560c670145ae4eaada78421
Galeofan <20Andrew12@mail.ru>
2024-03-19 19:17:58 +0300

    3. Арифметические операции. [#185]

--- src/main/java/ru/job4j/Main.java
+++ src/main/java/ru/job4j/Main.java
@@ -1,7 +1,18 @@
 package ru.job4j;
 
 public class Main {
+    @SuppressWarnings("checkstyle:EmptyLineSeparator")
     public static void main(String[] args) {
+        byte age = 27;
+        char sex = 'М';
+        float height = 2.00F;
+        short bricksCount = 10000;
+        int bacteriumCount = 20000000;
+        double distance = 1000000.00;
+        boolean workAllow = true;
+        long size = 4000000000000L;
+
         System.out.println("Привет, мир");
     }
+
 }
