294646eb341956f68f350804c5e3b1c4ce1ff602
Galeofan <20Andrew12@mail.ru>
2024-03-25 21:44:29 +0300

    1.1. Что такое import [#504773 #488073]

--- src/main/java/ru/job4j/math/MathFunction.java
+++ src/main/java/ru/job4j/math/MathFunction.java
@@ -3,10 +3,12 @@ package ru.job4j.math;
 public class MathFunction {
 
     public static double sum(double first, double second) {
+
         return first + second;
     }
 
     public static double multiply(double first, double second) {
+
         return first * second;
     }
 
@@ -15,6 +17,7 @@ public class MathFunction {
     }
 
     public static double divide(double first, double second) {
+
         return first / second;
     }
 }
