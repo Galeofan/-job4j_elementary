771f3452314e51d90e4747ba2f6f92718ddb8f94
Galeofan <20Andrew12@mail.ru>
2024-04-15 19:31:30 +0300

    6.0.3. Заполнение массива. [#226300]

--- src/main/java/ru/job4j/array/ArrayDefinition.java
+++ src/main/java/ru/job4j/array/ArrayDefinition.java
@@ -3,10 +3,20 @@ package ru.job4j.array;
 public class ArrayDefinition {
     public static void main(String[] args) {
         short[] ages = new short[10];
-        System.out.println(ages.length);
         String[] surnames = new String[100500];
-        System.out.println(surnames.length);
         float[] prices = new float[40];
+        String[] names = new String[4];
+        names[0] = "Petr Arsentev";
+        names[1] = "Test Testov";
+        names[2] = "Alex Alex";
+        names[3] = "Ivan Ivanov";
+
+        System.out.println(ages.length);
+        System.out.println(surnames.length);
         System.out.println(prices.length);
+        System.out.println(names[0]);
+        System.out.println(names[1]);
+        System.out.println(names[2]);
+        System.out.println(names[3]);
     }
 }
