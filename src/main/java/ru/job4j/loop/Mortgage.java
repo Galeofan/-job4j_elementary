7cb01b5032eede33f3892cfb3b1dc32f7eefbc58
Galeofan <20Andrew12@mail.ru>
2024-04-16 20:15:17 +0300

    5.7. Ипотека [#156315 #491314]

--- src/main/java/ru/job4j/loop/Mortgage.java
+++ src/main/java/ru/job4j/loop/Mortgage.java
@@ -4,9 +4,9 @@ public class Mortgage {
     public static int year(double amount, double salary, double percent) {
         int year = 0;
         percent /= 100;
-            while (amount + amount * percent > 0) {
+            while (amount > 0) {
                 year++;
-                amount = ((amount + amount * percent - salary) * percent) + amount + amount * percent - salary;
+                amount += amount * percent - salary;
                 System.out.println(amount);
             }
         return year;
