4d5c82d4e52c6bf0c9980a55c8c4d9974f146cfa
Galeofan <20Andrew12@mail.ru>
2024-04-24 21:09:46 +0300

    6.8. Метод двух указателей [#225]

---
+++ src/test/java/ru/job4j/array/TwoNumberSumTest.java
@@ -0,0 +1,44 @@
+package ru.job4j.array;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class TwoNumberSumTest {
+
+    @Test
+    void whenTwoEqualsNumbersYesTarget() {
+        int[] array = {5, 5};
+        int target = 10;
+        int[] result = TwoNumberSum.getIndexes(array, target);
+        int[] expected = {0, 1};
+        assertThat(result).isEqualTo(expected);
+    }
+
+    @Test
+    void whenTwoEqualsNumbersNoTarget() {
+        int[] array = {5, 5};
+        int target = 12;
+        int[] result = TwoNumberSum.getIndexes(array, target);
+        int[] expected = {};
+        assertThat(result).isEqualTo(expected);
+    }
+
+    @Test
+    void whenWithNegativeNumbersYesTarget() {
+        int[] array = {-7, -5, 0, 5, 8, 12};
+        int target = 3;
+        int[] result = TwoNumberSum.getIndexes(array, target);
+        int[] expected = {1, 4};
+        assertThat(result).isEqualTo(expected);
+    }
+
+    @Test
+    void whenWithoutNegativeNumbersYesTarget() {
+        int[] array = {1, 5, 10, 15, 16, 17};
+        int target = 15;
+        int[] result = TwoNumberSum.getIndexes(array, target);
+        int[] expected = {1, 2};
+        assertThat(result).isEqualTo(expected);
+    }
+}
